d17296fd42702e6709f598908029ceb5e3d146aa
Ryan <jijizaza999@gmail.com>
2020-10-17 19:28:09 -0700

    Updates

---
+++ precache-manifest.5e56c326e36cede064ce929cc437df9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f9f5b95fec439d89ff18cab0a614f6",
+    "url": "/fitnezone-store/index.html"
+  },
+  {
+    "revision": "6f13fa7a653faabe6187",
+    "url": "/fitnezone-store/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "244330bf4b6123978cc7",
+    "url": "/fitnezone-store/static/css/main.b6a17a53.chunk.css"
+  },
+  {
+    "revision": "6f13fa7a653faabe6187",
+    "url": "/fitnezone-store/static/js/2.61628d74.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/fitnezone-store/static/js/2.61628d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244330bf4b6123978cc7",
+    "url": "/fitnezone-store/static/js/main.fcf6c96e.chunk.js"
+  },
+  {
+    "revision": "2cc298770fda66b5d21a",
+    "url": "/fitnezone-store/static/js/runtime-main.1838c401.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88e68a8798cc39c020d9d1c8a88354b8.js"
+  "/fitnezone-store/precache-manifest.5e56c326e36cede064ce929cc437df9c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fitnezone-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
