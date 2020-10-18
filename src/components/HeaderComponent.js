910edfba29556227bb1edfcb75bcb04c8f8de30b
Ryan <jijizaza999@gmail.com>
2020-10-17 21:12:30 -0700

    images

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -45,9 +45,9 @@ function Header() {
     <React.Fragment>
       <Navbar dark sticky="top" expand="lg">
         <div className="container">
-          <NavbarBrand className="navbarBrand" href="#">
+          <NavbarBrand className="navbarBrand" href="fitnezone-store/#/home">
             <img
-              src="/assets/images/logo.png" alt=""
+              src="./assets/images/logo.png" alt=""
               height="50px"
               width="60px"
               alt="Fitnezone Logo"
