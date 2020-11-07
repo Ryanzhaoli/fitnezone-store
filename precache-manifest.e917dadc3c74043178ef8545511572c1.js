9372c50843e559330b57ddc98af52342286635e0
Ryan <jijizaza999@gmail.com>
2020-11-07 09:47:57 -0800

    Updates

---
+++ precache-manifest.e917dadc3c74043178ef8545511572c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5faebaecd652cee985a1236fe32a990",
+    "url": "/finezone-store/index.html"
+  },
+  {
+    "revision": "754124c7c3650f32368c",
+    "url": "/finezone-store/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "54c65c25365badc3873f",
+    "url": "/finezone-store/static/css/main.b6a17a53.chunk.css"
+  },
+  {
+    "revision": "754124c7c3650f32368c",
+    "url": "/finezone-store/static/js/2.5d9417a2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/finezone-store/static/js/2.5d9417a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c65c25365badc3873f",
+    "url": "/finezone-store/static/js/main.f0ae0b8e.chunk.js"
+  },
+  {
+    "revision": "eb0b82ddacf50c777dad",
+    "url": "/finezone-store/static/js/runtime-main.69c9dd8d.js"
+  }
+]);
