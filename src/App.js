a0b7a7976e670d6e30c612e5d2500c74bea2e51f
Ryan <jijizaza999@gmail.com>
2020-11-07 10:21:02 -0800

    img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './style/App.css';
-import { BrowserRouter as Router , Route , Switch, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router , Route , Switch} from 'react-router-dom';
 import Header from './components/HeaderComponent';
 import Footer from './components/FooterComponent';
 import Home from './components/HomeComponent';
 import Contact from './components/ContactComponent';
 import Basket from './components/BasketComponent';
-
-
 
 
 function App () {
