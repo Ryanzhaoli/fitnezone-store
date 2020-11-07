a5d14ecd52591518ac5eaaa8b7dc2ae8544747fa
Ryan <jijizaza999@gmail.com>
2020-11-07 09:40:09 -0800

    Updates

---
+++ precache-manifest.e0c0f328b11664351d801a81aaaa8158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f538b290dcca51e0b83943d9f4ddbc03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "754124c7c3650f32368c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c999358e0893652a869d",
+    "url": "/static/css/main.b6a17a53.chunk.css"
+  },
+  {
+    "revision": "754124c7c3650f32368c",
+    "url": "/static/js/2.5d9417a2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.5d9417a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c999358e0893652a869d",
+    "url": "/static/js/main.f89fc2fc.chunk.js"
+  },
+  {
+    "revision": "79fe81238694066ab722",
+    "url": "/static/js/runtime-main.02b29742.js"
+  }
+]);
